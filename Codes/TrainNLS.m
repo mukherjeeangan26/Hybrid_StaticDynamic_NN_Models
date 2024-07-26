e8cd36e1f556462049633b0c39ceb9f2183581bb
Angan Mukherjee <122834078+mukherjeeangan26@users.noreply.github.com>
2024-07-26 19:09:01 -0400

    Update TrainNLS.m

--- Codes/TrainNLS.m
+++ Codes/TrainNLS.m
@@ -2,11 +2,9 @@ function [ynn_t,nn_stat] = TrainNLS(Imat_t,dsr_t,nh,no)
 
 u = Imat_t;
 
-net_s = feedforwardnet(nh ,'trainlm');
+net_s = newff(Imat_t,dsr_t,[nh, no],{'logsig','logsig'},'trainlm');
 
-net_s.layers{1}.transferFcn = 'logsig';
-net_s.layers{2}.transferFcn = 'logsig';
-net_s.trainParam.max_fail = 2000;
+net_s.trainParam.max_fail = 1000;
 
 net_s = init(net_s);
 net_s.trainParam.epochs = 5000;
