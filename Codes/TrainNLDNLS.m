2b1e5a6f7c382f6817e7a62d56591d29356ed2b7
Angan Mukherjee <122834078+mukherjeeangan26@users.noreply.github.com>
2024-07-31 23:39:05 -0400

    Update TrainNLDNLS.m

--- Codes/TrainNLDNLS.m
+++ Codes/TrainNLDNLS.m
@@ -8,7 +8,7 @@ check1 = Inf;
 
 x_in = Imat_t;
 
-net_s = newff(Imat_t,dsr_t,[nh, no],{'logsig','purelin'},'trainlm');
+net_s = newff(Imat_t,dsr_t,nh,{'logsig','purelin'},'trainlm');
 net_s.trainParam.max_fail = 1000;
 
 % Initializing the Termination Criteria
